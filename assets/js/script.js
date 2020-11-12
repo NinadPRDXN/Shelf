634c58902fc05dc0be48d0c7112f9ebba2773b6e
NinadPRDXN <ninadp@prdxn.com>
2020-11-12 21:13:06 +0530

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,18 @@
 
 */
 
+var 
+hamburger = document.querySelector('.hamburger'),
+close = document.querySelector('nav ul li:last-child'),
+nav = document.querySelector('nav ul');
 
+hamburger.onclick = function() {
+    nav.style.transform = 'translateX(0)';
+}
+
+close.onclick = function() {
+    nav.style.transform = 'translateX(100%)';
+}
 
 
 
